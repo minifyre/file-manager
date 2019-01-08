3db93a5af6da1c6c7e0523cf2035b1af0ea40c0a
Minifyre <minifyre@gmail.com>
2019-01-07 20:10:25 -0600

    +curry & curryN for ease of use

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import silo from './node_modules/silo/index.js'
 import truth from './node_modules/truth/truth.mjs'
 import v from './node_modules/v/v.mjs'
 
-const {config,util,logic,output,input}=silo
+const
+{config,util,logic,output,input}=silo,
+{curry,curryN}=util,
 //@todo find & replace "custom" & "element" with proper values
 export default silo(async function(initialState,url='/node_modules/custom-element/')
 {
