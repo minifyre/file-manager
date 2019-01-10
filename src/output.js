a39af9103f1aa11326fd8374aa4ed6859f9cad71
Minifyre <minifyre@gmail.com>
2019-01-09 19:24:52 -0600

    +output.icon
    This puts the file extension on the icon. In the future an icon font will provide an appropriate symbol based on the given file extension.

--- src/output.js
+++ src/output.js
@@ -1,1 +1,7 @@
 output.desc=(state,item)=>v('span.desc',{},file.url2name(item.path))
+output.icon=function(state,item)
+{
+	const ext=file.url2ext(item.path)||'folder'
+
+	return v('button.icon',{data:{ext,pointerup:'toggleSelect'}},ext)
+}
